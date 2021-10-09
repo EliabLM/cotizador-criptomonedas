cee549bb2c4b16d0daf0556300e2bc9a5fb000b1
eliablm <eliablopez@hotmail.com>
2021-10-08 22:18:27 -0500

    Agrega formulario y objeto con las divisas

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+	// State con la información seleccionada
+	const [selecciondivisa, setSeleccionDivisa] = useState('');
+	const [seleccioncripto, setSeleccionCripto] = useState('');
+
+	const DIVISAS = [
+		{
+			codigo: 'USD',
+			nombre: 'Dolar de Estados Unidos',
+		},
+		{
+			codigo: 'EUR',
+			nombre: 'Euro',
+		},
+		{
+			codigo: 'COP',
+			nombre: 'Peso Colombiano',
+		},
+	];
+
 	return (
 		<form>
-			Soy un formulario
+			<label>Divisa</label>
+			<select onChange={(e) => setSeleccionDivisa(e.target.value)}>
+				<option value="">-- Seleccione su divisa --</option>
+				{DIVISAS.map((opcion) => (
+					<option key={opcion.codigo} value={opcion.codigo}>
+						{opcion.nombre}
+					</option>
+				))}
+			</select>
+			<label>Criptomoneda</label>
+			<select>
+				<option value="">-- Selecione la criptomoneda --</option>
+			</select>
 			<button type="submit">Calcular</button>
 		</form>
 	);
