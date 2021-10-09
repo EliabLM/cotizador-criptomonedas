47473ef28769d820520e49458c5962498a2626b5
eliablm <eliablopez@hotmail.com>
2021-10-08 21:21:55 -0500

    Agrega estructura inicial y primeros estilos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import Formulario from './components/Formulario';
+
+import bitcoin from './assets/undraw_bitcoin2_ave7.svg';
+
 function App() {
-	return <div className="App">cotizador de criptos</div>;
+	return (
+		<div className="contenedor">
+			<div>
+				<img className="imagen" src={bitcoin} alt="Bitcoin" />
+			</div>
+			<div>
+				<h1>Cotizador de Criptomonedas</h1>
+				<Formulario />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Formulario = () => {
+	return (
+		<form>
+			Soy un formulario
+			<button type="submit">Calcular</button>
+		</form>
+	);
+};
+
+export default Formulario;
